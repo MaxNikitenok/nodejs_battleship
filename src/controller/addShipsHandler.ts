7a5404a91050d34e712e0ebeba6dce738ad16d07
Max_Nikitenok <xxx250790@gmail.com>
2023-07-11 02:01:52 +0300

    feat: shot sequence

--- src/controller/addShipsHandler.ts
+++ src/controller/addShipsHandler.ts
@@ -1,15 +1,33 @@
 import { getRoomShips, getUsers } from './../dataBase/dataBase';
 import { addShips } from '../dataBase/dataBase';
 import { wss } from '../ws_server';
+import { IUser } from '../types';
+
+export let firstAttacker: number;
+
+const getFirstAttacker = (users: IUser[]) => {
+
+  const firstAttackerIndex = () => {
+    if (Math.random() < 0.49) {
+      return 0;
+    } else {
+      return 1;
+    }
+  };
+  return users[firstAttackerIndex()].index;
+};
+
 
 export const addShipsHandler = async (data: string) => {
-  const parsedData = JSON.parse(data)
+  const parsedData = JSON.parse(data);
 
   addShips(parsedData);
 
   const roomShips = getRoomShips(parsedData.gameId);
 
   const users = getUsers();
+
+  firstAttacker = getFirstAttacker(users)
 
   function sendAll() {
     for (let i = 0; i < users.length; i++) {
@@ -34,7 +52,7 @@ export const addShipsHandler = async (data: string) => {
         JSON.stringify({
           type: 'turn',
           data: JSON.stringify({
-            currentPlayer: 0,
+            currentPlayer: firstAttacker,
           }),
           id: 0,
         })
--- src/controller/attackHandler.ts
+++ src/controller/attackHandler.ts
@@ -1,89 +1,131 @@
 import { getRoom, getShipInfo, getUserBySocket } from '../dataBase/dataBase';
 import { wss } from '../ws_server';
+import { firstAttacker } from './addShipsHandler';
 
-export const attackHandler = (
-  data: string,
-  socket: import('ws')
-) => {
-  console.log(data);
+let flag: number | null = null;
+
+export const attackHandler = (data: string, socket: import('ws')) => {
   const parsedData = JSON.parse(data);
 
   const room = getRoom(parsedData.gameId);
 
+  if (flag === null) flag = firstAttacker;
+
+  console.log('attacker', firstAttacker);
+
   const prevShooter = getUserBySocket(socket);
+  console.log('flag', flag, '\n', 'shooter', prevShooter.index);
+  if (prevShooter.index !== flag) return;
   const currentShooter = room.roomUsers.find((user) => {
     const shooter = user.index !== prevShooter.index;
     return shooter;
   });
 
+  const shootResult = getShipInfo(parsedData, currentShooter.index);
+
   wss.clients.forEach((client) => {
     client.send(
       JSON.stringify({
-        type: 'turn',
+        type: 'attack',
         data: JSON.stringify({
-          currentPlayer: currentShooter.index,
+          position: {
+            x: shootResult.x,
+            y: shootResult.y,
+          },
+          currentPlayer: prevShooter.index,
+          status: shootResult.status,
         }),
         id: 0,
       })
     );
   });
-  
 
-  const shootResult = getShipInfo(parsedData, currentShooter.index)
+  if (shootResult.status === 'miss') {
+    flag = currentShooter.index;
+    console.log('currentShooter.index', currentShooter.index);
 
+    wss.clients.forEach((client) => {
+      client.send(
+        JSON.stringify({
+          type: 'turn',
+          data: JSON.stringify({
+            currentPlayer: currentShooter.index,
+          }),
+          id: 0,
+        })
+      );
+    });
+  }
+
+  if (shootResult.status === 'shot') {
+    flag = prevShooter.index;
+
+    wss.clients.forEach((client) => {
+      client.send(
+        JSON.stringify({
+          type: 'turn',
+          data: JSON.stringify({
+            currentPlayer: prevShooter.index,
+          }),
+          id: 0,
+        })
+      );
+    });
+  }
 
   console.log(shootResult);
 
-  if(shootResult.status === 'killed') {
+  if (shootResult.status === 'killed') {
+    flag = prevShooter.index;
 
-    shootResult.freeArea.forEach(item=>{
-      socket.send(
+    shootResult.freeArea.forEach((item) => {
+      wss.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            type: 'attack',
+            data: JSON.stringify({
+              position: {
+                x: item.x,
+                y: item.y,
+              },
+              currentPlayer: prevShooter.index,
+              status: 'miss',
+            }),
+            id: 0,
+          })
+        );
+      });
+    });
+
+    shootResult.shipPositions.forEach((item) => {
+      wss.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            type: 'attack',
+            data: JSON.stringify({
+              position: {
+                x: item.x,
+                y: item.y,
+              },
+              currentPlayer: prevShooter.index,
+              status: 'killed',
+            }),
+            id: 0,
+          })
+        );
+      });
+    });
+
+    wss.clients.forEach((client) => {
+      client.send(
         JSON.stringify({
-          type: 'attack',
+          type: 'turn',
           data: JSON.stringify({
-            position: {
-              x: item.x,
-              y: item.y,
-            },
             currentPlayer: prevShooter.index,
-            status: 'miss',
           }),
           id: 0,
         })
       );
-    })
-
-    shootResult.shipPositions.forEach(item=>{
-      socket.send(
-        JSON.stringify({
-          type: 'attack',
-          data: JSON.stringify({
-            position: {
-              x: item.x,
-              y: item.y,
-            },
-            currentPlayer: prevShooter.index,
-            status: 'killed',
-          }),
-          id: 0,
-        })
-      );
-    })
+    });
   }
-
-
-  socket.send(
-    JSON.stringify({
-      type: 'attack',
-      data: JSON.stringify({
-        position: {
-          x: shootResult.x,
-          y: shootResult.y,
-        },
-        currentPlayer: prevShooter.index,
-        status: shootResult.status,
-      }),
-      id: 0,
-    })
-  );
 };
